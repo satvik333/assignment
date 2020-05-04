da82feae7c8a3a8279c026bd0f3c0526668832ec
satvik333 <55409095+satvik333@users.noreply.github.com>
2020-05-04 11:28:08 +0530

    Add files via upload

---
+++ a2.py
@@ -0,0 +1,3 @@
+a=input("Enter 1st name")
+b=input("Enter last name")
+print((b)[::-1]+" "+(a)[::-1])
---
+++ a7.py
@@ -0,0 +1,1 @@
+print("WE, THE PEOPLE OF INDIA,\n\thaving solemnly resolved to constitute India into a SOVEREIGN,! \n\t\tSOCIALIST, SECULAR, DEMOCRATIC REPUBLIC \n\t\t and to secure to all its citizens")
